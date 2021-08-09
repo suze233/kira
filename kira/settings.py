8f7e278c60540dcb9121031cb33b9528de15f0e4
SuZe <kunliang.me@outlook.com>
2021-08-10 01:34:20 +0800

    创建用户表

--- kira/settings.py
+++ kira/settings.py
@@ -12,7 +12,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -23,7 +22,6 @@ SECRET_KEY = '9^#u4n&tt!gy!2)!fnb0l@ps-b^rgm%(bir--e2b1xp&w!gey#'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -68,7 +66,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'kira.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -82,7 +79,6 @@ DATABASES = {
         'NAME': 'suze_blog',  # 数据库名称
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -102,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -115,7 +110,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
@@ -191,4 +185,4 @@ LOGGING = {
 }
 
 # 替换系统User
-# AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = 'users.User'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+# 用户信息
+class User(AbstractUser):
+    # 手机
+    mobile = models.CharField(max_length=11, unique=True, blank=False)
+
+    # 头像
+    avatar = models.ImageField(upload_to='avatar/%Y/%m/%d', blank=True)
+
+    # 简介
+    user_desc = models.CharField(max_length=500, blank=True)
+
+    class Meta:
+        db_table = 'tb_users'  # 修改表名
+        verbose_name = '用户管理'
+        verbose_name_plural = verbose_name  # admin后台显示
+
+    def __str__(self):
+        return self.mobile
